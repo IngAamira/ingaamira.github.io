46cb9b328adca3113d90843cb108304993ec39ba
ingaamira <andres.mira@outlook.com>
2024-01-11 14:12:55 -0500

    Add test render img with firebase

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { TranslationModule } from '../../../shared/modules/translation.module';
 import { TranslationService } from '../../../shared/services/translation.service';
@@ -6,14 +7,25 @@ import { TranslationService } from '../../../shared/services/translation.service
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ TranslationModule ],
+  imports: [TranslationModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  getUsa: string = '';
+  getSpain: string = '';
 
-  constructor(private translationService: TranslationService) {
+  constructor(
+    private translationService: TranslationService,
+    private http: HttpClient
+  ) {}
 
+  ngOnInit(): void {
+    this.getUsa =
+      'https://firebasestorage.googleapis.com/v0/b/website-ingaamira-dev.appspot.com/o/assets%2Ficons%2Fusa.png?alt=media&token=d7c735aa-2ca7-48e7-aed3-66136adba6b6';
+
+    this.getSpain =
+      'https://firebasestorage.googleapis.com/v0/b/website-ingaamira-dev.appspot.com/o/assets%2Ficons%2Fspain.png?alt=media&token=f042f1ac-4b0d-48f8-87d4-0a624b87b12b';
   }
 
   changeLanguage(lang: string): void {
