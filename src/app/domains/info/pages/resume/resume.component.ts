5841d2efebb9eb598d8a31f2beeacdba3700ab77
ingaamira <andres.mira@outlook.com>
2024-01-13 08:23:02 -0500

    Updtae menu home and resume with ngFor

--- src/app/domains/info/pages/resume/resume.component.ts
+++ src/app/domains/info/pages/resume/resume.component.ts
@@ -3,6 +3,16 @@ import { Title } from '@angular/platform-browser';
 
 import { TranslationService } from '../../../shared/services/translation.service';
 
+interface MenuItem {
+  name : string;
+  event: () => void;
+}
+
+interface Objective {
+  id: number;
+  translationKey: string;
+}
+
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
@@ -23,6 +33,22 @@ export class ResumeComponent {
   ) {
     this.titleService.setTitle('Resume');
   }
+
+  public menuItems: MenuItem[] = [
+    { name: 'DOWNLOAD_PDF_RESUME_DEV',  event: () => this.DownloadFileDev()  },
+    { name: 'DOWNLOAD_PDF_RESUME_DATA', event: () => this.DownloadFileData() },
+  ];
+
+  public objectives: Objective[] = [
+    { id: 1, translationKey: 'IAS_SD_OBJECTIVE_1' },
+    { id: 2, translationKey: 'IAS_SD_OBJECTIVE_2' },
+    { id: 3, translationKey: 'IAS_SD_OBJECTIVE_3' },
+    { id: 4, translationKey: 'IAS_SD_OBJECTIVE_4' },
+    { id: 5, translationKey: 'IAS_SD_OBJECTIVE_5' },
+    { id: 6, translationKey: 'IAS_SD_OBJECTIVE_6' },
+    { id: 7, translationKey: 'IAS_SD_OBJECTIVE_7' },
+    { id: 8, translationKey: 'IAS_SD_OBJECTIVE_8' }
+  ];
 
   DownloadFileDev() {
     const link = this.renderer.createElement('a');
--- src/app/domains/shared/components/navbar/navbar.component.ts
+++ src/app/domains/shared/components/navbar/navbar.component.ts
@@ -1,13 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLinkWithHref, RouterLinkActive } from '@angular/router';
+import { RouterModule, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
 
 import { TranslationModule } from '../../../shared/modules/translation.module';
 import { TranslationService } from '../../../shared/services/translation.service';
 
+interface MenuItem {
+  route: string;
+  img: string;
+}
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ RouterLinkWithHref, RouterLinkActive, TranslationModule ],
+  imports: [ CommonModule, RouterModule, RouterLinkWithHref, RouterLinkActive, TranslationModule ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
@@ -16,6 +22,13 @@ export class NavbarComponent {
   constructor(private translationService: TranslationService) {
 
   }
+
+  public menuItems: MenuItem[] = [
+    { route: '/',          img: 'bi bi-house-door-fill'  },
+    { route: '/portfolio', img: 'bi bi-briefcase-fill'   },
+    { route: '/resume',    img: 'bi bi-person-workspace' },
+    { route: '/contact',   img: 'bi bi-person-fill-add'  },
+  ];
 
   changeLanguage(lang: string): void {
     this.translationService.changeLanguage(lang);
