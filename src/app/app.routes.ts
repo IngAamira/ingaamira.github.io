ddd65c89c72a1f2c6c2b728446dbb8a408cbbe97
ingaamira <andres.mira@outlook.com>
2026-03-18 06:24:58 -0500

    Add portfolio routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,39 +3,18 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from '@presentation/shared/components/layout/layout.component';
 import { NotFoundComponent } from '@presentation/features/portfolio/pages/not-found/not-found.component';
 
+import { portfolioRoutes } from '@presentation/features/portfolio/portfolio.routes';
+
 export const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
     children: [
+      ...portfolioRoutes,
       {
-        path: '',
-        loadComponent: () =>
-          import('@presentation/features/portfolio/pages/home/home.component')
-            .then(m => m.HomeComponent),
-      },
-      {
-        path: 'portfolio',
-        loadComponent: () =>
-          import('@presentation/features/portfolio/pages/portfolio/portfolio.component')
-            .then(m => m.PortfolioComponent),
-      },
-      {
-        path: 'resume',
-        loadComponent: () =>
-          import('@presentation/features/portfolio/pages/resume/resume.component')
-            .then(m => m.ResumeComponent),
-      },
-      {
-        path: 'contact',
-        loadComponent: () =>
-          import('@presentation/features/portfolio/pages/contact/contact.component')
-            .then(m => m.ContactComponent),
-      },
-    ],
-  },
-  {
-    path: '**',
-    component: NotFoundComponent,
-  },
+        path: '**',
+        component: NotFoundComponent,
+      }
+    ]
+  }
 ];
